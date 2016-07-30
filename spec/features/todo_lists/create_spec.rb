3b278043f0785d29ea182f4115ee018395f6e2d6
Jay Wang <jaywang007@yahoo.com>
2016-07-30 17:07:29 -0400

    Test codes and add validation

---
+++ spec/features/todo_lists/create_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe "Creating todo lists" do
+  it "redirect to the todo list index page on success" do
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title", with: "My todo list"
+    fill_in "Description", with: "Daily todo list."
+    click_button "Create Todo list"
+
+    #expect this page to have content as above
+    expect(page).to have_content("My todo list")
+  end
+
+  it "displays an error when the todo list has no title" do
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title", with: ""
+    fill_in "Description", with: "Daily todo list."
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("This is my daily todo list.")
+  end
+end
